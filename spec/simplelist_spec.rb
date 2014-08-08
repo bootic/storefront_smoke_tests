39d537c75cdeee8d79c37ad3edd72b72f7d33553
Ismael Celis <ismaelct@gmail.com>
2014-08-08 16:52:37 +0100

    Capybara setup, tested simplelist.bootic.dev homepage

---
+++ spec/simplelist_spec.rb
@@ -0,0 +1,75 @@
+require 'spec_helper'
+
+describe 'simplelist.bootic.dev', type: :feature do
+  before :all do
+    Capybara.app_host = 'http://simplelist.bootic.dev'
+  end
+
+
+  describe 'homepage' do
+    before do
+      visit '/'
+    end
+
+    it 'shows highlighted products' do
+      expect(page).to have_content("Pony Blue")
+      expect(page).to have_content("Buzzy")
+      expect(page).to have_content("Stallion II")
+      expect(page).to have_content("Lightrek")
+      expect(page).to have_content("Este es un producto de ejemplo en Bootic")
+    end
+
+    it 'has main navigation' do
+      expect(page).to have_link("Despachos")
+      expect(page).to have_link("Contacto")
+      expect(page).to have_link("Acerca de nosotros")
+      expect(page).to have_link("Productos")
+      expect(page).to have_link("Blog")
+    end
+
+    it 'has catalog links' do
+      expect(page).to have_content('Catálogo de productos')
+      expect(page).to have_link("Accesorios")
+      expect(page).to have_link("Bicicletas")
+      expect(page).to have_link("Destacados")
+      expect(page).to have_link("Ofertas")
+    end
+
+    it 'starts with an empty cart' do
+      within '.ajax_cart' do
+        expect(page).to have_content 'Carro vacío'
+      end
+    end
+
+    describe 'managing the Ajax cart' do
+      before do
+        within '.products .item-1' do
+          click_on 'Agregar al carro'
+        end
+      end
+
+      it 'adds product to the cart' do
+        within '.ajax_cart' do
+          expect(page).not_to have_content 'Carro vacío'
+          expect(page).to have_content('(Default) Pony Blue')
+          expect(page).to have_content('$120.000')
+        end
+      end
+
+      it 'can update quantity' do
+        within '.ajax_cart ul.items li' do
+          fill_in 'quantity', with: 2
+          page.execute_script('$("input[name=quantity]").blur()') # blur
+          expect(page).to have_content('$240.000')
+        end
+      end
+
+      it 'can remove product from the cart' do
+        within '.ajax_cart' do
+          click_on '-'
+          expect(page).not_to have_content('(Default) Pony Blue')
+        end
+      end
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,25 @@
+require 'rspec'
+require 'capybara/rspec'
+
+# Add the 'spec' path in the load path
+spec_dir = File.dirname(__FILE__)
+$LOAD_PATH.unshift(spec_dir)
+
+# Require all ruby files in the 'support' folder
+Dir[File.join(spec_dir, "support/**/*.rb")].each {|f| require f}
+
+# RSpec config here
+RSpec.configure do |config|
+
+  # Capybara config here
+  Capybara.configure do |capybara|
+    # Don't run a rack app
+    capybara.run_server = false
+
+    # I'm using the mechanize driver but your free to use your favorite one (env-js, selenium, ...)
+    capybara.default_driver = :selenium
+  end
+
+  # Don't forget to tell to RSpec to include Capybara :)
+  config.include Capybara::DSL
+end
